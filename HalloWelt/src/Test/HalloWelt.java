f0c6d97bfc1c85894ab752b167e8b1c696896d1c
gmuy <guenter.muley@bilfinger.com>
2018-09-25 15:59:55 +0200

    - Change Hallo Welt.java to direct output - create new class Hello - create new instance Hello and implement output from Hello.java

--- HalloWelt/src/Test/HalloWelt.java
+++ HalloWelt/src/Test/HalloWelt.java
@@ -3,8 +3,14 @@ package Test;
 public class HalloWelt {
 
 	public static void main(String[] args) {
-		// Display Hello World on screen
+		// new instance
+		Hello test = new Hello();
+		
+		// Display Hello World on screen (direct output)
 		System.out.println("Hello World!");
+		
+		// Display Hallo Welt on screen (from class Hello)
+		test.sayHello();
 	}
 
 }
---
+++ HalloWelt/src/Test/Hello.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class Hello {
+	void sayHello() {
+		// Display Hallo Welt on screen
+		System.out.println("Hallo Welt!");
+	}
+}
